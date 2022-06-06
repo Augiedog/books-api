f638688f48667d86bf508687d05d05dbf38bee98
Augie Bren <Augie>
2022-06-05 23:47:33 -0500

    goonight

--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -5,7 +5,7 @@ function Def(html) {
         <html>
             <head>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"></link>
-                <link rel="stylesheet" href="style.css"></link>
+                <link rel="stylesheet" href="/style.css"></link>
                 <title>{html.title || 'Books'}</title>
             </head>
             <body>
