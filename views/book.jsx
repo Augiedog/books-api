d44225ab5139130eec817678885af187cc534fc3
Augie Bren <Augie>
2022-06-06 22:29:15 -0500

    added cors

--- views/book.jsx
+++ views/book.jsx
@@ -1,10 +1,10 @@
 const React = require('react')
 const Def = require('./layouts/default')
 
-function Book({ books, title }) {
+function Book({ books }) {
     books.map((book) => {
         return (
-            <Def title={title}>
+        <Def title={book.title}>
             <h2>{book.title}</h2>
 
         </Def>
